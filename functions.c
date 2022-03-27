f0a5992d96d958b31d32f5551cc91bee2a5662f9
lucas <lss16@inf.ufpr.br>
2022-03-27 00:19:25 -0300

    Update newton method

--- functions.c
+++ functions.c
@@ -2,6 +2,18 @@
 
 void NewtonResolveMethod(infos in)
 {
+    void *f;
+    double *x_ant = in.initialsApproaches;
+    double *x = (double *) malloc (sizeof(double)*in.n);
+    
+    f = evaluator_create(in.f);
+    assert(f);
+
+    printf("-- NewtonResolveMethod --\n");
+    for ( int i = 0 ; i < in.itMax ; ++i )
+    {
+        
+    }
 
 }
 
