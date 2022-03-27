e9425f2b8ff4aacb8bf355f4b9ef48b76b3406ce
lucas <lss16@inf.ufpr.br>
2022-03-27 10:38:59 -0300

    Update getinfos

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "functions.h"
 
-void GetInfos(infos *in, int *countProblems)
+infos *GetInfos(int *countProblems)
 {
+
+    infos *in = (infos *) malloc (sizeof(infos)*(*countProblems));
     int length;
 	
     while ( scanf("%d",&in[*countProblems-1].n) == 1)
@@ -26,39 +28,15 @@ void GetInfos(infos *in, int *countProblems)
         ++(*countProblems);
         in = realloc (in,sizeof(infos)*(*countProblems)-1);
     }
-    
 }
 
 int main (int argc, char *argv[])
 {
     int countProblems = 1 ;	
-    infos *in = (infos *) malloc (sizeof(infos)*countProblems); 
     char *arqName = validateArgs(argc,argv);
 
-    int length;
-	
-    while ( scanf("%d",&in[countProblems-1].n) == 1)
-    {
- 
-        in[(countProblems)-1].initialsApproaches = (double *) malloc (sizeof(double)*in[(countProblems)-1].n);
-        in[(countProblems)-1].times = (timesInfos *) malloc (sizeof(timesInfos));
-        
-        scanf("%s",in[(countProblems)-1].f);
+    infos *in = GetInfos(&countProblems); 
 
-        length = strlen(in[(countProblems)].f);
-
-        if (length > 0 && in[(countProblems)].f[length-1] == '\n')
-            in[(countProblems)].f[length-1] = '\0';
-       
-        for (int i = 0 ; i < in[(countProblems)-1].n; ++i)
-            scanf("%lf",&in[(countProblems)-1].initialsApproaches[i]);
-        
-        scanf("%lf",&in[(countProblems)-1].epsilon);
-        scanf("%d",&in[(countProblems)-1].itMax);
- 
-        ++(countProblems);
-        in = realloc (in,sizeof(infos)*(countProblems)-1);
-    }
 
     for ( int i = 0 ; i < countProblems ; ++i )
     {
