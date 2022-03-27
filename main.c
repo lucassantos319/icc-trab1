247a031ebb448c0f557d4ec40fc1502c455b1b86
lucas <lss16@inf.ufpr.br>
2022-03-27 01:05:55 -0300

    update get infos

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include "functions.h"
 
-void getInfos(infos *in,int *countProblems)
+void GetInfos(infos **in, int *countProblems)
 {
+    int length;
+	
+    while ( scanf("%d",&in[*countProblems-1]->n) == 1)
+    {
+ 
+        in[(*countProblems)-1]->initialsApproaches = (double *) malloc (sizeof(double)*in[(*countProblems)-1]->n);
+        in[(*countProblems)-1]->times = (timesInfos *) malloc (sizeof(timesInfos));
+        
+        scanf("%s",in[(*countProblems)-1]->f);
+
+        length = strlen(in[(*countProblems)]->f);
+
+        if (length > 0 && in[(*countProblems)]->f[length-1] == '\n')
+            in[(*countProblems)]->f[length-1] = '\0';
+       
+        for (int i = 0 ; i < in[(*countProblems)-1]->n; ++i)
+            scanf("%lf",&in[(*countProblems)-1]->initialsApproaches[i]);
+        
+        scanf("%lf",&in[(*countProblems)-1]->epsilon);
+        scanf("%d",&in[(*countProblems)-1]->itMax);
+ 
+        ++(*countProblems);
+    	in = realloc (in,sizeof(infos)*(*countProblems)-1);
+    }
+    
 }
 
 int main (int argc, char *argv[])
@@ -10,26 +35,7 @@ int main (int argc, char *argv[])
     infos *in = (infos *) malloc (sizeof(infos)*countProblems); 
     char *arqName = validateArgs(argc,argv);
 
-    getInfos(in,&countProblems); 
-    int length;
-	
-    while ( scanf("%d",&in[countProblems-1].n) == 1)
-    {
- 
-        in[countProblems-1].initialsApproaches = (double *) malloc (sizeof(double)*in[countProblems-1].n);
-        in[countProblems-1].times = (timesInfos *) malloc (sizeof(timesInfos));
-        
-        scanf("%s",in[countProblems-1].f);
-
-        for (int i = 0 ; i < in[countProblems-1].n; ++i)
-            scanf("%lf",&in[countProblems-1].initialsApproaches[i]);
-        
-        scanf("%lf",&in[countProblems-1].epsilon);
-        scanf("%d",&in[countProblems-1].itMax);
- 
-        ++countProblems;
-    	in = realloc (in,sizeof(infos)*countProblems-1);
-    }
+    GetInfos(&in,&countProblems);
     
     for ( int i = 0 ; i < countProblems ; ++i )
     {
