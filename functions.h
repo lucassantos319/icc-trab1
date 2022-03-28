c37e1343350d3320c8d1c284bd2239999d952daa
lucas <lss16@inf.ufpr.br>
2022-03-28 19:50:50 -0300

    OK

--- functions.h
+++ functions.h
@@ -4,9 +4,8 @@
 #include "utils.h"
 
 void NewtonResolveMethod(infos in);
+double **GetMatrix(infos in, double *x,int type);
 void NewtonModifyResolveMethod(infos in);
-double **GetFMatrix(infos in, double *x);
-double **GetFDMatrix(infos in, double *x);
 void NewtonGaussSeidelResolveMethod(infos in);
 void printResult(infos *in, int countProblems,char *arqName);
 
