4e3f704fbc8f212f8fb72784fc95c3ce78429592
lucas <lss16@inf.ufpr.br>
2022-03-28 19:44:32 -0300

    Update matrix and function d

--- functions.c
+++ functions.c
@@ -6,7 +6,7 @@ void NewtonResolveMethod(infos in)
     double *x_ant = in.initialsApproaches;
     double *x = (double *) malloc (sizeof(double)*in.n);
 
-    double **mF = GetFMatrix(in,x_ant);
+    double **mF = GetFMatrix(in,x_ant), **mFD = GetFDMatrix(in,x_ant);
     if ( mF == NULL  )
         return ;
 
@@ -55,6 +55,52 @@ double **GetFMatrix(infos in, double *x)
         {
             fd = evaluator_derivative(f,variables[i]);
             mF[i][j] = evaluator_evaluate(f,in.n,variables,x);
+        }
+
+    for (int i = 0 ; i < in.n ; ++i)
+    {
+        for (int j = 0 ; j < in.n ; ++j)
+            printf("%g ",mF[i][j]);
+
+        printf("\n");
+    }
+
+	
+    printf("\n");
+    free(variables);
+    return mF;
+
+}
+
+double **GetFDMatrix(infos in, double *x)
+{
+    void *f,*fd,*fdd;
+    int count;
+    char **variables;
+    
+    double **mF = (double **) malloc (sizeof(double *)*in.n);
+
+    for ( int i = 0 ; i < in.n ; ++i )
+        mF[i] = (double *) malloc (sizeof(double) * in.n);
+
+    f = evaluator_create(in.f);
+    int length = strlen(in.f);
+
+    if ( in.f[length-1] == '\n' && length > 0 )
+        in.f[length-1] = '\0';
+    
+    assert(f);
+    evaluator_get_variables (f, &variables, &count);
+
+    if ( count != in.n )
+        return NULL;
+
+    // change method to get what function wants 
+    for ( int i = 0 ; i < in.n ; ++i )
+        for (int j = 0 ; j < in.n ; ++j )
+        {
+            fd = evaluator_derivative(f,variables[i]);
+            mF[i][j] = evaluator_evaluate(fd,in.n,variables,x);
         }
 
 
--- functions.h
+++ functions.h
@@ -3,9 +3,10 @@
 
 #include "utils.h"
 
-double **GetFMatrix(infos in, double *x);
 void NewtonResolveMethod(infos in);
 void NewtonModifyResolveMethod(infos in);
+double **GetFMatrix(infos in, double *x);
+double **GetFDMatrix(infos in, double *x);
 void NewtonGaussSeidelResolveMethod(infos in);
 void printResult(infos *in, int countProblems,char *arqName);
 
